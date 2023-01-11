677279a9a136e5176192684655ac69001e09a00c
Cristian Llanos <cristian.llanos@gmail.com>
2023-01-10 21:17:39 -0300

    first commit

---
+++ src/main/java/cl/bci/bciexercise/BciexerciseApplication.java
@@ -0,0 +1,13 @@
+package cl.bci.bciexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BciexerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/bci/bciexercise/BciexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bci.bciexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
